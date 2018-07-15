2e10a6d378ef6cb5dc5f1c127fd746b736787d57
Tomasz <edisoner97@gmail.com>
2018-07-15 12:32:47 +0100

    0.1

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default () => {
+  return (
+    <div>
+      <button>Siema jestem ostrym byczkiem</button>
+    </div>
+  )
+}
+
--- client/src/routes/Home.js
+++ client/src/routes/Home.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+import Navbar from '../components/Navbar';
 
 export default class Home extends Component {
   render() {
     return (
       <div>
         Siema tutaj Home
+        <Navbar />
+        <Navbar />
+        <Navbar />
       </div>
     )
   }
